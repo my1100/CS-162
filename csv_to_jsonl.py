f248a614812e3e5ea25f7e57d66f5392c427691f
Aditya Patil <158780307+AdityaPatil-R@users.noreply.github.com>
2025-05-15 19:04:27 -0700

    Update csv to jsonl code

--- csv_to_jsonl.py
+++ csv_to_jsonl.py
@@ -7,10 +7,5 @@ df = pd.read_csv('AI_Human.csv')
 # Open a .jsonl file to write
 with open('kaggle.jsonl', 'w') as f:
     for _, row in df.iterrows():
-        if row['generated'] == 0:
-            json_obj = {"human_text": row['text']}
-        elif row['generated'] == 1:
-            json_obj = {"machine_text": row['text']}
-        else:
-            continue  # skip invalid values
+        json_obj = {"text": row['text'], "generated": row['generated']}
         f.write(json.dumps(json_obj) + '\n')
